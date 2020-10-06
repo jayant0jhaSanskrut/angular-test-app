f7a1bf152022573f965a143858699589b337df35
jayant0jhaSanskrut <jayant.ojha@sanskrut.in>
2020-10-06 22:35:14 +0530

    feat: Add view1, view2 and nav components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'angular-test-app/one', component: View1Component },
+  { path: 'angular-test-app/two', component: View2Component },
+  { path: 'angulartestapp', redirectTo: 'angular-test-app/one' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
